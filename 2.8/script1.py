cba97559e4fce7eb314284544d7ce14b9d088287
Idan Retyk <idaniretyk@gmail.com>
2024-01-01 17:56:18 +0200

    almost final 2.8

--- 2.8/script1.py
+++ 2.8/script1.py
@@ -1,40 +1,52 @@
 import socket
 
 #global
-cont = True
+
 
 
 def clinet(port):
+    cont = True
     c = socket.socket()
     c.connect(("127.0.0.1",port))
-    print("connected to port " + port)
+    print("connected to port " + str(port))
     st = input("")
     if (st == "exit"):
         cont = False
     print("A sent " + st)
-    c.send(st)
+    c.send(st.encode())
     print("disconnecting")
     c.close()
+    return cont
     
 def server(port):
+    cont = True
     server = socket.socket()
     server.bind(("127.0.0.1",port))
-    print("A listeing in port " + port)
+    print("A listeing in port " + str(port))
+    server.listen(100)
     c,a = server.accept()
     data = c.recv(1024)
     if (data == "exit".encode()):
         cont = False
-    print("A recived " + data)
-        
+    print("A recived " + data.decode())
+    c.close()
+    server.close()
+    return cont
+
+
     
 def main():
     port = 8000
+    cont = True
     while (cont):
-        clinet(port)
-        if (cont):
-            server(port)
+        cont = clinet(port)
+        if (not cont):
+            break
+        port += hash(port) % 101
+        cont = server(port)
         
-        port += hash(port) % 100
+        for i in range(100): #its very unlikly the port will stay the same after this
+            port += hash(port) % (i + 1)
 
 if __name__ == "__main__":
     main()
--- 2.8/script2.py
+++ 2.8/script2.py
@@ -1,41 +1,50 @@
 import socket
-import random
 
-#global
-cont = True
+
 
 
 def clinet(port):
+    cont = True
     c = socket.socket()
     c.connect(("127.0.0.1",port))
-    print("connected to port " + port)
+    print("connected to port " + str(port))
     st = input("")
     if (st == "exit"):
         cont = False
     print("B sent " + st)
-    c.send(st)
+    c.send(st.encode())
     print("disconnecting")
     c.close()
+    return cont
     
 def server(port):
+    cont = True
     server = socket.socket()
     server.bind(("127.0.0.1",port))
-    print("B listeing in port " + port)
+    print("B listeing in port " + str(port))
+    server.listen(10)
     c,a = server.accept()
     data = c.recv(1024)
     if (data == "exit".encode()):
         cont = False
-    print("B recived " + data)
-        
+    print("B recived " + data.decode())
+    server.close()
+    return cont
+
+
     
 def main():
     port = 8000
+    cont = True
     while (cont):
-        server(port)
-        if (cont):
-            clinet(port)
+        cont = server(port)
+        if (not cont):
+            break
+        port += hash(port) % 101
+        cont = clinet(port)
         
-        port += hash(port) % 100
+        for i in range(100): #its very unlikly the port will stay the same after this
+            port += hash(port) % (i + 1)
 
 if __name__ == "__main__":
     main()
