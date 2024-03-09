bfce220e35465627e9d787064a3c23bcaf7c267e
Idan Retyk <idaniretyk@gmail.com>
2024-03-09 14:55:00 +0200

    improved ns look up

---
+++ improved_nslu.py
@@ -0,0 +1,63 @@
+from scapy.all import UDP,DNS,DNSQR,IP,sr1,DNSRR
+
+import re
+
+
+def ns_look_up(ip):
+    domain = ip
+
+
+    dns_query = IP(dst="8.8.8.8") / UDP(dport=53) / DNS(qdcount=1, rd=1,qd = 0)/DNSQR(qname=domain)
+    response = sr1(dns_query,verbose=0)
+    handle(response,domain)
+
+        
+
+def is_valid(domain):
+    return re.match(r"www.\w+(.\w+)+",domain) != None
+
+
+
+def handle(response_packet,qname):
+    number_of_dns_response = response_packet[DNS].ancount
+    dnsrr_list = response_packet.an
+    
+    # Check if the response contain Canonical name
+    if dnsrr_list[0].type == 5:
+        print("Name:   ", dnsrr_list[0].rdata.decode())
+        print("Address:  ", end="")
+        # Print all dnsrrs
+        for dnsrr in dnsrr_list[1:]:
+            print(dnsrr.rdata)
+        print("Aliases: ", qname)
+    else:  # if does not contain Canonical name
+        print("Name:   ", qname)
+        print("Address:  ", end="")
+        # Print all dnsrrs
+        for dnsrr in dnsrr_list:
+            if (dnsrr != None):
+                print(dnsrr.rdata)  
+
+
+
+def main():
+    
+    while True:
+        quit = False
+        data = input("> ")
+        
+        if (data.lower() in ("quit()", "exit()")):
+            print()
+            break
+        
+        while not is_valid(data):
+            print("Please enter a valid domain")
+            data = input("> ")
+            if (data.lower() in ("quit()", "exit()")):
+                quit = True
+        if (not quit):
+            ns_look_up(data)
+        
+
+if __name__ == "__main__":
+    main()
--- nslu.py
+++ nslu.py
@@ -1,15 +1,10 @@
-from scapy.layers.inet import IP
-from scapy.all import DNS, DNSQR, IP, sr1, UDP
+from scapy.all import UDP,DNS,DNSQR,IP,sr1,DNSRR
+domain = input("Enter addr \n")
 
 
-def recv_packet(domain: str):
-    dns_packet = IP(dst='8.8.8.8')/UDP(sport=24601,dport=53)/DNS(qd=None,qdcount=1,rd=1,)/DNSQR(qname=domain)
-    dns_packet[DNSQR] = DNSQR(qname="www.google.com")
-    dns_packet.show()
-    response_packet = sr1(dns_packet)
-    return response_packet
-    
-
-respone = recv_packet("www.google.com")
-print(respone)
-
+dns_query = IP(dst="8.8.8.8") / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname=domain))
+response = sr1(dns_query)
+if response[DNSRR].type == 1: 
+    print(response[DNSRR].rdata)
+elif response[DNSRR].type == 5:
+    print(response[DNSRR][1].rdata)
