d8cc5e74bc8d276dcb24e6dec96550d057217ad3
Idan Retyk <idaniretyk@gmail.com>
2024-09-06 23:35:06 +0300

    python challenge level 13 done:

--- python/challenge.py
+++ python/challenge.py
@@ -1,1 +1,9 @@
-#python challenge - currently level 12
+#python challenge - currently level 14 "italy"
+
+from PIL import Image
+
+img = Image.open("python/wire.png", 'r')
+
+bytes_arr = img.load()
+
+print(bytes_arr[0,0])
