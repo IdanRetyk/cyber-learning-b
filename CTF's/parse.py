82f0eef0ec84be597dc7ecfadf15f8491766fdfc
Idan Retyk <idaniretyk@gmail.com>
2024-10-03 15:49:22 +0300

    added stack

--- CTF's/parse.py
+++ CTF's/parse.py
@@ -3,11 +3,15 @@ import sys
 with open(sys.argv[1],'r') as file:
     lines = file.readlines()
     
-pubiku_stack = list()
+# pubiku_stack = list()
+stack = [None] * 100
+sp = 0
 for line in lines:
     if "chu" in line:
-        pubiku_stack.append(line.split()[-1])
-    if "pabiku !x" in line or "pabiku !a" in line:
-        head = pubiku_stack.pop()
-    if "pika" in line:
-        print(head)
+        # push 
+        sp += 1
+    #     pubiku_stack.append(line.split()[-1])
+    # if "pabiku !x" in line or "pabiku !a" in line:
+    #     head = pubiku_stack.pop()
+    # if "pika" in line:
+    #     print(head)
