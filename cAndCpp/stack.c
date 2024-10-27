f19d7f8ea898c826f76ac51e2e7a4c46ff37cbbf
Idan Retyk <idaniretyk@gmail.com>
2024-10-27 14:47:34 +0200

    started nodes in c

---
+++ cAndCpp/stack.c
@@ -0,0 +1,26 @@
+//stack data-structre
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int val;
+    struct Node *next;
+}Node ;
+
+int main(){
+    Node* node1 = ( Node*)malloc(sizeof(Node));
+    Node* node2 = ( Node*)malloc(sizeof(Node));
+    node1->val = 5;
+    node1->next = node2;
+    node2->val = 20;
+    node2->next = NULL;
+
+    Node* temp = node1;
+    while (temp){
+        printf("%d\n", temp-> val);
+        temp = temp -> next;
+    }
+
+    return 0;
+}
